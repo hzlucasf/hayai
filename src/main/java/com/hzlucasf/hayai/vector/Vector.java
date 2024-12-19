c68cd16d9567820327e8dd03a80946683c4f980e
hzlucasf <hzlucasf@gmail.com>
2024-12-19 18:41:49 -0300

    add Vector

---
+++ src/main/java/com/hzlucasf/hayai/vector/Vector.java
@@ -0,0 +1,11 @@
+package com.hzlucasf.hayai.vector;
+
+public class Vector<T> {
+    private T[] elements;
+
+    private int length;
+
+    public int getLength() {
+        return length;
+    }
+}
