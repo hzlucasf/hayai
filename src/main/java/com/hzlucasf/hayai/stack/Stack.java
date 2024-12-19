3acf9ddfb5ef0732c2186aed4763bc1922c0b96a
hzlucasf <hzlucasf@gmail.com>
2024-12-19 14:46:09 -0300

    add Stack

---
+++ src/main/java/com/hzlucasf/hayai/stack/Stack.java
@@ -0,0 +1,13 @@
+package com.hzlucasf.hayai.stack;
+
+import com.hzlucasf.hayai.node.Node;
+
+public class Stack<T> {
+    private Node<T> top;
+
+    private int length;
+
+    public int getLength() {
+        return length;
+    }
+}
