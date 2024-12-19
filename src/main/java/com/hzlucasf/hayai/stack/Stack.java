75479fa108322c7783658e37d0555f19531fc92b
hzlucasf <hzlucasf@gmail.com>
2024-12-19 14:52:34 -0300

    add method to push element on stack

--- src/main/java/com/hzlucasf/hayai/stack/Stack.java
+++ src/main/java/com/hzlucasf/hayai/stack/Stack.java
@@ -14,4 +14,14 @@ public class Stack<T> {
     public boolean isEmpty() {
         return length == 0;
     }
+
+    public void push(T element) {
+        if (element == null) {
+            throw new IllegalArgumentException("Element cannot be null");
+        }
+
+        top = new Node<>(element, top);
+
+        length += 1;
+    }
 }
