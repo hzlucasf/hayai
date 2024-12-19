a50cc71edfc1698f694351e1d5f9257e1e88701f
hzlucasf <hzlucasf@gmail.com>
2024-12-19 18:24:15 -0300

    add Queue

---
+++ src/main/java/com/hzlucasf/hayai/queue/Queue.java
@@ -0,0 +1,15 @@
+package com.hzlucasf.hayai.queue;
+
+import com.hzlucasf.hayai.node.Node;
+
+public class Queue<T> {
+    private Node<T> start;
+
+    private Node<T> end;
+
+    private int length;
+
+    public int getLength() {
+        return length;
+    }
+}
